f20bd0928d814479e36eab235f8a414d80b575d6
escritacontabilidade <comercial@escritacontabilidade.com.br>
2026-06-06 17:02:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ from openpyxl import load_workbook
 from proposal_builder_v2 import gerar_pdf_proposta_comercial_v2
 from proposal_html_builder import gerar_pdf_proposta_html
 from email_sender import enviar_email_proposta
+from radar_module import tela_radar
 
 def autenticar_usuario(usuario, senha):
     try:
