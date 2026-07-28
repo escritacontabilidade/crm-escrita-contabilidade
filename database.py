97418a89c6905037cbc04df04631da8a73b57eda
escritacontabilidade <comercial@escritacontabilidade.com.br>
2026-07-28 15:43:54 -0300

    Update database.py

--- database.py
+++ database.py
@@ -97,8 +97,7 @@ def get_perguntas_por_origem(origem):
     )
     return res.data if res.data else []
 
-    return float(res.data[0]["horas_esforco"])
-
+    
 def get_regras_precificacao():
     supabase = get_supabase()
 
