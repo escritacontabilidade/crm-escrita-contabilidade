867817415a42f552bcfdf7a90bc118754b516057
escritacontabilidade <comercial@escritacontabilidade.com.br>
2026-07-30 14:38:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ from radar_module import tela_radar, tela_cliente_radar
 from export_precificacao import tela_exportar_matriz_precificacao
 from grupos_module import tela_grupos_economicos
 from analista_ia import tela_analista_ia
+from admin_precificacao import tela_admin_precificacao
 
 def autenticar_usuario(usuario, senha):
     try:
